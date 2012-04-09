d623f549cb96bbc4df504770642bc8cef9df09d1
Juha Paananen <juha.paananen@reaktor.fi>
2012-04-09 21:49:29 +0300

    add more stuff to the examples.js to start with

--- examples.js
+++ examples.js
@@ -1,4 +1,10 @@
 function show(value) { $("#big").text(value != undefined ? value.toString() : "") }
+function keyUps(keyCode) { return $("body").asEventStream("keyup").filter(function(e) { return e.keyCode == keyCode }) }
+function keyDowns(keyCode) { return $("body").asEventStream("keydown").filter(function(e) { return e.keyCode == keyCode }) }
+function keyState(keyCode, downValue, upValue) { return keyDowns(keyCode).map(downValue).merge(keyUps(keyCode).map(upValue)).toProperty(upValue) }
+
+function v(x, y) { return new Vector2D(x, y) }
+var v0 = v(0, 0)
 
 $(function() {
 
